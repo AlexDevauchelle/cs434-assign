703bedca1295dbf23e60be91902d5296732abaae
Alex <70631774+AlexDevauchelle@users.noreply.github.com>
2023-09-08 13:00:20 +0200

    Completion of recfun - 100%

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -1,6 +1,8 @@
 package recfun
 import common._
 
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
@@ -14,15 +16,35 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (r < 0 || c > r || c<0) 0
+    else if (r == 0) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    @tailrec
+    def recBalance(chars: List[Char], x: Int): Boolean = {
+      if (x < 0) return false
+      if (chars.isEmpty) return x==0
+      val char = chars.head
+      if (char == '(') recBalance(chars.tail,x + 1)
+      else if (char == ')') recBalance(chars.tail,x - 1)
+      else recBalance(chars.tail,x)
+    }
+    recBalance(chars,0)
+  }
+
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
